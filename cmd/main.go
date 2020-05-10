42e2a82fd7cb10fb4e894e2b28f977efa675555a
Sergey <xxsmotur@gmail.com>
2020-05-11 01:41:41 +0500

    Add loading of the config and add examples

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/saromanov/epit/pkg/epit"
+	"go.uber.org/zap"
+)
+
+func main() {
+	log, err := zap.NewProduction()
+	if err != nil {
+		panic("unable to init logging")
+	}
+	if err := epit.LoadConfig("./examples/basic/.epit.yml"); err != nil {
+		log.Fatal(fmt.Sprintf("unable to parse config: %v", err))
+	}
 }
--- pkg/epit/parse.go
+++ pkg/epit/parse.go
@@ -1,1 +1,25 @@
 package epit
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/saromanov/cowrow"
+)
+
+// LoadConfig provides loading of configuration file
+func LoadConfig(path string) error {
+
+	if path == "" {
+		return errors.New("path to config is empty")
+	}
+
+	cfg := map[string]interface{}{}
+	err := cowrow.LoadByPath(path, &cfg)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(cfg)
+	return nil
+}
