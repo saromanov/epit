92a68ec01695cef3d2eda85edc83d72d218a21de
Sergey <xxsmotur@gmail.com>
2020-05-16 23:59:28 +0500

    Checking of the stage name before execution

--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/saromanov/epit/pkg/epit"
 	"go.uber.org/zap"
 )
 
 func main() {
+	args := os.Args
+	if len(args) == 1 {
+		log.Fatal("name of the stage is not defined")
+	}
 	log, err := zap.NewProduction()
 	if err != nil {
 		panic("unable to init logging")
 	}
-	if err := epit.ExecStage("./examples/basic/.epit.yml", "dev"); err != nil {
+	if err := epit.ExecStage("./examples/basic/.epit.yml", args[1]); err != nil {
 		log.Fatal(fmt.Sprintf("unable to parse config: %v", err))
 	}
 }
