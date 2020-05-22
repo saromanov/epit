6d9ecf3abe2b505a43babf09622106589d25deb0
Sergey <xxsmotur@gmail.com>
2020-05-22 09:34:09 +0500

    Additional check on unput variables

--- pkg/epit/exec.go
+++ pkg/epit/exec.go
@@ -9,17 +9,26 @@ import (
 
 // ExecStage provides execution of the stage
 func ExecStage(logger *zap.Logger, path, name string) error {
+	if logger == nil {
+		return fmt.Errorf("ExecStage: logger is not defined")
+	}
+	if path == "" {
+		return fmt.Errorf("ExecStage: path is not defined")
+	}
+	if name == "" {
+		return fmt.Errorf("ExecStage: name of the stage is not defined")
+	}
 	cfg, err := loadConfig(path)
 	if err != nil {
 		return fmt.Errorf("ExecStage: unable to load config: %v", err)
 	}
 	stage, ok := cfg[name]
 	if !ok {
-		return fmt.Errorf("name of the stage is not found")
+		return fmt.Errorf("ExecStage: name of the stage is not found")
 	}
 	st := Config{}
 	if err := mapstructure.Decode(stage, &st); err != nil {
-		return fmt.Errorf("unable to decode structure: %v", err)
+		return fmt.Errorf("ExecStage: unable to decode structure: %v", err)
 	}
 
 	return run(logger, name, st)
